9452fa7f7758ee96fef4e4a850a9a154a02ef4d8
mrtkyz <mrtkyz@hotmail.com>
2017-07-29 11:55:32 +0300

    added project

---
+++ AOPVSMSample/Aspects/LogAttribute.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using PostSharp.Aspects;
+using System.Diagnostics;
+
+namespace AOPVSMSample.Aspects
+{
+    [Serializable]
+    public class LogAttribute : OnMethodBoundaryAspect
+    {
+        public override void OnEntry(MethodExecutionArgs args)
+        {
+            Trace.Write(string.Format("Method başladı... {0}", args.Method));
+            Trace.Write(" Parametreler:");
+
+            for (int i = 0; i < args.Arguments.Count; i++)
+            {
+                Trace.Write(" " + args.Arguments[i]);
+                Trace.WriteIf(i != args.Arguments.Count - 1, ",");
+            }
+
+            Trace.Write(Environment.NewLine);
+        }
+
+
+        public override void OnExit(MethodExecutionArgs args)
+        {
+            Trace.WriteLine(string.Format("method bitti, {0}. cevap: {1}", args.Method, args.ReturnValue));
+        }
+
+
+        public override void OnException(MethodExecutionArgs args)
+        {
+            Trace.WriteLine(String.Format("Metod hata oluştu. {0}: {1}", args.Method.Name,
+                args.Exception.StackTrace));
+            args.FlowBehavior = FlowBehavior.Continue;
+        }
+    }
+}
---
+++ AOPVSMSample/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace AOPVSMSample
+{
+    class Program
+    {
+        static void Main()
+        {
+
+            Hesaplama calc = new Hesaplama();
+
+            double bolum = calc.Bol(100, 0);
+
+            double toplam = calc.Ekle(1, 1);
+        }
+    }
+}
